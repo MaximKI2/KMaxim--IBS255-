3519efdc4a038645ff11777ae3e8b7858f23172f
MaximKI2 <125002009+MaximKI2@users.noreply.github.com>
2023-02-14 11:24:26 +0300

    IBS 255
    14.02.2023
    Практическая работа №1

---
+++ main(1).py
@@ -0,0 +1,29 @@
+tseloe = 10
+drobnoe = 8.4
+stroka = "No"
+big_Netseloe = tseloe * 3.5
+drobnoe -= 1
+tseloe / drobnoe
+big_Netseloe / drobnoe
+stroka = "No"*2 + '_' + "yes"*3
+
+print (drobnoe)
+print (big_Netseloe)
+print (stroka)
+print (drobnoe)
+print (tseloe)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
